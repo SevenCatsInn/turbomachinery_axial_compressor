ec540697f0aa1265d358eedad536f4fb17d50dbe
f_vac <vaccarifrancesco2@gmail.com>
2022-07-28 10:38:30 +0200

    Changes in MLD for stage 2

--- turboproject_S2.py
+++ turboproject_S2.py
@@ -11,11 +11,11 @@ R = c_p * (gamma-1)/gamma # Gas constant [J/(kg K)]
 
 #Input
 mdot=100 #mass flow rate
-Pt1=p_t3[mean_index] # pressure [bar]
-Tt1=T_t3[mean_index] #inlet temperature [K]
+Pt3=p_t3[mean_index] # pressure [bar]
+Tt3=T_t3[mean_index] #inlet temperature [K]
 beta=1.45/beta #compression ratio
 Rm=0.30 #mean line radius
-rho1=Pt1/(R*Tt1)
+rho3=Pt3/(R*Tt3)
 Q=mdot/rho1
 
 
@@ -30,109 +30,102 @@ eta_R = 0.92
 Um = U[mean_index]
 
 #determine loading
-L_is=cp*Tt1*(beta**((gamma-1)/gamma)-1)
+L_is=cp*Tt3*(beta**((gamma-1)/gamma)-1)
 L_eul=L_is/efficiency_TT
 
 psi = L_eul / Um**2
 lamda=psi*2
 omega=rpm * pi / 30
-V1a=V_a3[mean_index]
-phi = V1a / Um
-V1t=V_t3[mean_index]
 
-V1=[V1a, V1t]
-V1_mag=Norm(V1)
-W1a=V1a
-W1t=V1t-Um
-W1=[W1a, W1t]
-W1_mag=Norm(W1)
-beta1=arctan(W1[1]/W1[0])
+V3a=V_a3[mean_index]
+phi = V3a / Um
+V3t=V_t3[mean_index]
 
-T1=Tt1-V1_mag**2/(2*cp)
-M1=V1_mag/sqrt(gamma*R*T1)
-p1=Pt1*(1+(gamma-1)/2*M1**2)**((-gamma)/(gamma-1))
+V3=[V3a, V3t]
+V1_mag=Norm(V3)
+W3a=V3a
+W3t=V3t-Um
+W3=[W3a, W3t]
+W3_mag=Norm(W3)
+beta3=arctan(W3t/W3a)
 
-#quantities at station 2 (after rotor)
-V2t=L_eul/Um + V1t
-Tt2=L_eul/cp + Tt1
-W2_mag=sqrt(W1_mag**2-xi*L_eul*2)
-W2t=V2t-Um
-W2a=sqrt(W2_mag**2-W2t**2)
-W2=[W2a, W2t]
+T3=Tt3-V3_mag**2/(2*cp)
+M3=V3_mag/sqrt(gamma*R*T3)
+p3=Pt3*(1+(gamma-1)/2*M3**2)**((-gamma)/(gamma-1))
 
-V2a=W2a
+#quantities at station 4 (after rotor)
+V4t=L_eul/Um + V3t
+Tt4=L_eul/cp + Tt3
+W4_mag=sqrt(W3_mag**2-xi*L_eul*2)
+W4t=V4t-Um
+W4a=sqrt(W4_mag**2-W4t**2)
+W4=[W4a, W4t]
+
+V4a=W4a
 err=20
 i=0
 
 while abs(err) > 10**(-4):
-    V2_mag=sqrt(V2a**2+V2t**2)
-    T2=Tt2-V2_mag**2/(2*cp)
-    T2is=T1+eta_R*(T2-T1)
-    p2=(T2is/Tt1)**(gamma/(gamma-1))*Pt1
-    rho2=p2/(R*T2)
-    V2a_new=mdot/(rho2*2*pi*b1*Rm)
-    err=abs(V2a_new-V2a)
-    V2a=V2a_new
-    M2 = V2_mag / sqrt(gamma * R * T2)
-    Pt2 = p2 * (1 + (gamma-1)/2 * M2**2)**(gamma/(gamma-1))
+    V4_mag=sqrt(V4a**2+V4t**2)
+    T4=Tt4-V4_mag**2/(2*cp)
+    T4is=T4+eta_R*(T4-T3)
+    p4=(T4is/Tt3)**(gamma/(gamma-1))*Pt3
+    rho4=p4/(R*T4)
+    V4a_new=mdot/(rho4*2*pi*b1*Rm)
+    err=abs(V4a_new-V4a)
+    V4a=V4a_new
+    M4 = V4_mag / sqrt(gamma * R * T4)
+    Pt4 = p4 * (1 + (gamma-1)/2 * M4**2)**(gamma/(gamma-1))
     i=i+1
 
 
-W2a=V2a
-W2=[W2a, W2t]
-W2_mag_new=sqrt(W2a**2+W2t**2)
-beta2=arctan(W2t/W2a)
-V2=[V2a, V2t]
-xi_new=(V1a**2-V2a**2+V1t**2-V2t**2+2*Um*(V2t-V1t))/(2*L_eul)
-xi=(W1_mag**2-W2_mag_new**2)/(2*L_eul) 
+W4a=V4a
+W4=[W4a, W4t]
+W4_mag_new=Norm(W4)
+beta4=arctan(W4t/W4a)
+V4=[V4a, V4t]
+# xi_new=(V1a**2-V2a**2+V1t**2-V2t**2+2*Um*(V2t-V1t))/(2*L_eul)
+xi=(W3_mag**2-W4_mag_new**2)/(2*L_eul) 
 
 
 # Mean line design for the stator
 
-alpha2 = arctan(V2t/V2a)
-alpha3 = 0 * pi/180# Design choice
+alpha4 = arctan(V4t/V4a)
+alpha5 = 0 * pi/180# Design choice
 
-print(cos(alpha3) / cos(alpha2), "> 0.72 ?") 
+print(cos(alpha5) / cos(alpha4), "> 0.72 ?") 
 
-V3a = V2a
-V3t = V3a * tan(alpha3)
-Tt3 = Tt2 # Imposed by thermodynamics, no work in stator
+V5a = V4a
+V5t = V5a * tan(alpha5)
+Tt5 = Tt4 # Imposed by thermodynamics, no work in stator
 
 err = 1e10
 tol = 0.0001
 iter = 0
 
 while abs(err)>tol:
-    V3_mag=sqrt(V3a**2+V3t**2)
-    T3=Tt3-V3_mag**2/(2*cp)
-    T3is=T2 + eta_S*(T3-T2)
-    p3=(T3is/Tt2)**(gamma/(gamma-1))*Pt2
-    rho3=p3/(R*T3)
-    V3a_new=mdot/(rho3*2*pi*b1*Rm)
-    err=abs(V3a_new-V3a)
-    V3a=V3a_new
+    V5_mag=sqrt(V5a**2+V5t**2)
+    T5=Tt5-V5_mag**2/(2*cp)
+    T5is=T4 + eta_S*(T5-T4)
+    p5=(T5is/Tt4)**(gamma/(gamma-1))*Pt4
+    rho5=p5/(R*T5)
+    V5a_new=mdot/(rho5*2*pi*b1*Rm)
+    err=abs(V5a_new-V5a)
+    V5a=V5a_new
     iter=iter+1
-V3 = [V3a, V3t]
+V5 = [V5a, V5t]
 
 # Change variables names for radial equilibrium script
-T_1m  = T1
-p_1m  = p1
-V_a1m = V1a
-V_t1m = V1t
-T_2m  = T2
-p_2m  = p2
-V_a2m = V2a
-V_t2m = V2t
-T_3m  = T3
-p_3m  = p3
-V_a3m = V3a
-V_t3m = V3t
-b_1 = b1
-R_m = Rm
-rpm = RPM
+
 
 # print(beta)
 # print("phi,psi,chi = ", phi,psi,xi_new)
 
-print(xi_new)
-print(T_1m,p_1m,V_a1m,V_t1m,T_2m,p_2m,V_a2m,V_t2m,T_3m,p_3m,V_a3m,V_t3m,b_1,R_m,rpm)
+T_4m  = T4
+p_4m  = p4
+V_a4m = V4a
+V_t4m = V4t
+T_5m  = T5
+p_5m  = p5
+V_a5m = V5a
+V_t5m = V5t
