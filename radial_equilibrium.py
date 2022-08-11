162fe30bad4e415d813f4561c44320b19282f426
f_vac <vaccarifrancesco2@gmail.com>
2022-08-11 17:31:58 +0200

    moved blade design algorithm to its own function

--- radial_equilibrium.py
+++ radial_equilibrium.py
@@ -5,6 +5,7 @@ exec(open("./turboproject.py").read()) # Run mean line design
 import numpy as np
 import matplotlib.pyplot as plt
 from naca65_plotter import *
+from blade_design import *
 
 plt.rcParams.update({"text.usetex": True})
 
@@ -597,158 +598,35 @@ while abs(err) > tol: # Begin loop to get mass flow convergence
 
 
 
-
-
-
-
-
-
-
 ############### Blade design (Stage 1) ##############
 
-# Design Parameters from computations above
-# Inlet
-beta_1root = beta_1[0]          * 180 / pi #deg
-beta_1mid  = beta_1[mean_index] * 180 / pi #deg
-beta_1tip  = beta_1[-1]         * 180 / pi #deg
+percent_th1 = 10               # [%] Max thickness WRT chord of blade profile 
+chord1      = 0.08             # [m] Starting point from reference procedure
+solidity1   = 1.3              # [ ] ! Initial assumption at midspan
+theta1 = [33, 26, 19]
 
-# Outlet
-beta_2root = beta_2[0]          * 180 / pi #deg
-beta_2mid  = beta_2[mean_index] * 180 / pi #deg
-beta_2tip  = beta_2[-1]         * 180 / pi #deg
-
-# Deflection
-deltabeta1_root = beta_1root - beta_2root
-deltabeta1_mid  = beta_1mid  - beta_2mid 
-deltabeta1_tip  = beta_1tip  - beta_2tip 
-
-# Initial hypothesis ----> NOTE: TUNABLE
-percent_th = 10               # [%] Max thickness WRT chord of blade profile 
-chord      = 0.08             # [m] Starting point from reference procedure
-th = chord * percent_th / 100 # [m] Actual thickness
-solidity   = 1.3              # [ ] ! Initial assumption at midspan
-
-
-s_mid = solidity * chord
-
-n_blade = round( 2 * np.pi * R_m / s_mid) # Number of blades
-
-# Pitch along blade span
-s_mid  = 2 * np.pi * R_m / n_blade
-s_tip  = 2 * np.pi * R_t / n_blade
-s_root = 2 * np.pi * R_h / n_blade
-
-# Solidity along blade span, recomputed after choosing n blades
-sigma_mid  = chord / s_mid
-sigma_tip  = chord / s_tip
-sigma_root = chord / s_root
-
-# Equivalent camber theta: from graphs on slide 9 ppt
-# NOTE: TUNABLE
-theta_eq_root = 33
-theta_eq_mid  = 26
-theta_eq_tip  = 19
-
-
-# compute C_l = theta/25
-cl_mid  = theta_eq_mid  / 25
-cl_tip  = theta_eq_tip  / 25
-cl_root = theta_eq_root / 25
-
-
-# TODO: Insert semiempirical correlations below instead of graph values
-
-# Zero camber incidence angle
-
-# Semiempirical exponents
-p_mid  = 0.914 + sigma_mid **3 / 160
-p_tip  = 0.914 + sigma_tip **3 / 160
-p_root = 0.914 + sigma_root**3 / 160
-
-# Incidence angles in degrees
-i0_10_mid  = abs(beta_1mid)**p_mid   / (5 + 46*np.exp(-2.3 * sigma_mid))  - 0.1 * sigma_mid**3  * np.exp((abs(beta_1mid)-70)/4)
-i0_10_tip  = abs(beta_1tip)**p_tip   / (5 + 46*np.exp(-2.3 * sigma_tip))  - 0.1 * sigma_tip**3  * np.exp((abs(beta_1tip)-70)/4) 
-i0_10_root = abs(beta_1root)**p_root / (5 + 46*np.exp(-2.3 * sigma_root)) - 0.1 * sigma_root**3 * np.exp((abs(beta_1root)-70)/4)
-
-
-# Correction factor for profile thickness
-q = 0.28 / (0.1 + (th/chord)**0.3) # Empirical exponent
-
-k_ith_mid  = (10*th/chord)**q
-k_ith_tip  = (10*th/chord)**q
-k_ith_root = (10*th/chord)**q
-
-
-# Slope factor n
-n_mid  = 0.025 * sigma_mid  - 0.06 - ( (abs(beta_1mid)/90)  ** (1+1.2*sigma_mid)  ) / (1.5 + 0.43 * sigma_mid)
-n_tip  = 0.025 * sigma_tip  - 0.06 - ( (abs(beta_1tip)/90)  ** (1+1.2*sigma_tip)  ) / (1.5 + 0.43 * sigma_tip)
-n_root = 0.025 * sigma_root - 0.06 - ( (abs(beta_1root)/90) ** (1+1.2*sigma_root) ) / (1.5 + 0.43 * sigma_root)
-
-
-# Optimal Incidence Angle
-i_opt_mid  = i0_10_mid  * k_ith_mid  + n_mid  * theta_eq_mid
-i_opt_tip  = i0_10_tip  * k_ith_tip  + n_tip  * theta_eq_tip
-i_opt_root = i0_10_root * k_ith_root + n_root * theta_eq_root
-
-# Deviation Angle
-delta0_mid  = 0.01 * sigma_mid  * abs(beta_1mid)  + (0.74*sigma_mid**1.9 + 3 * sigma_mid) * (abs(beta_1mid) / 90) ** (1.67 + 1.09 * sigma_mid) 
-delta0_tip  = 0.01 * sigma_tip  * abs(beta_1tip)  + (0.74*sigma_tip**1.9 + 3 * sigma_tip) * (abs(beta_1tip) / 90) ** (1.67 + 1.09 * sigma_tip)  
-delta0_root = 0.01 * sigma_root * abs(beta_1root) + (0.74*sigma_root**1.9 + 3 * sigma_root) * (abs(beta_1root) / 90) ** (1.67 + 1.09 * sigma_root) 
-
-
-# Correction for thickness effects on deviation
-kdeltath_mid  = 6.25 * (th/chord) + 37.5 * (th/chord)**2    
-kdeltath_tip  = 6.25 * (th/chord) + 37.5 * (th/chord)**2   
-kdeltath_root = 6.25 * (th/chord) + 37.5 * (th/chord)**2  
-
-
-# Exponent Factor b
-b_mid  = 0.9625 - 0.17 * abs(beta_1mid)/100 - 0.85 * (abs(beta_1mid)/100)**3
-b_tip  = 0.9625 - 0.17 * abs(beta_1tip)/100 - 0.85 * (abs(beta_1tip)/100)**3
-b_root = 0.9625 - 0.17 * abs(beta_1root)/100 - 0.85 * (abs(beta_1root)/100)**3
-
-
-# Slope factor m - m1 = Reference value for solidity = 1
-m1_mid  = 0.17 - 0.0333 * abs(beta_1mid) /100 + 0.333 * (beta_1mid/100) **2
-m1_tip  = 0.17 - 0.0333 * abs(beta_1tip) /100 + 0.333 * (beta_1tip/100) **2
-m1_root = 0.17 - 0.0333 * abs(beta_1root)/100 + 0.333 * (beta_1root/100)**2
-
-
-m_mid  = m1_mid  / (sigma_mid  **b_mid )
-m_tip  = m1_tip  / (sigma_tip  **b_tip )
-m_root = m1_root / (sigma_root **b_root)
-
-
-#computation of deviation
-delta_mid = delta0_mid * kdeltath_mid + m_mid * theta_eq_mid / (sigma_mid ** b_mid)
-delta_tip = delta0_tip * kdeltath_tip + m_tip * theta_eq_tip / (sigma_tip ** b_tip)
-delta_root = delta0_root * kdeltath_root + m_root * theta_eq_root / (sigma_root ** b_root)
-
-
-#final delta beta
-deltabetafinal_mid = theta_eq_mid - delta_mid + i_opt_mid
-deltabetafinal_tip = theta_eq_tip - delta_tip + i_opt_tip
-deltabetafinal_root = theta_eq_root - delta_root + i_opt_root
-
+inc1, dev1, deltaBeta1 = lieblein_design(beta_1,beta_2,percent_th1,chord1,solidity1, theta1, rr)
 
 print("")
-print("Lieblein deflection ROOT = ", deltabetafinal_root)
-print("Design deflection   ROOT = ", abs(deltabeta1_root))
+print("STAGE 1 BLADE DESIGN")
+print("")
+print("Lieblein deflection ROOT = ", deltaBeta1[0])
+print("Design deflection   ROOT = ", abs(180/np.pi*(beta_1[0]-beta_2[0])))
 
 print("")
-print("Lieblein deflection MID = ", deltabetafinal_mid)
-print("Design deflection   MID = ", abs(deltabeta1_mid))
+print("Lieblein deflection MID = ", deltaBeta1[1])
+print("Design deflection   MID = ", abs(180/np.pi*(beta_1[mean_index]-beta_2[mean_index])))
 
 print("")
-print("Lieblein deflection TIP = ", deltabetafinal_tip)
-print("Design deflection   TIP = ", abs(deltabeta1_tip))
+print("Lieblein deflection TIP = ", deltaBeta1[2])
+print("Design deflection   TIP = ", abs(180/np.pi*(beta_1[-1]-beta_2[-1])))
 
-# input()
+input()
 
-for theta, beta, inc, color in zip([theta_eq_root,theta_eq_mid, theta_eq_tip], [beta_1root,beta_1mid,beta_1tip], [i_opt_root,i_opt_mid,i_opt_tip], ['c','b','k']):
-    angle = - beta + inc
+for theta, beta, inc, color in zip(theta1, [beta_1[0],beta_1[mean_index],beta_1[-1]], inc, ['c','b','k']):
+    stagger = - beta + inc
 
-    Xc,Yc,Ux,Uy,Lx,Ly, profile_name = naca65(theta, percent_th/100 , chord, [0.03,0.0023], angle )
+    Xc,Yc,Ux,Uy,Lx,Ly, profile_name = naca65(theta, percent_th/100 , chord, [0.03,0.0023], stagger )
 
     
     # plt.plot(Xc,Yc,'-.',color='r', linewidth=1)
@@ -759,6 +637,7 @@ for theta, beta, inc, color in zip([theta_eq_root,theta_eq_mid, theta_eq_tip], [
     plt.grid(alpha=0.2)
 
     # plt.title(profile_name)
+
 plt.legend(["Hub","","Mean","","","Tip"])
 plt.title("Stage 1")
 plt.show()
