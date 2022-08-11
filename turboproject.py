9ff9ca1278f034274c79b1b9596eeedca4d6bb90
f_vac <vaccarifrancesco2@gmail.com>
2022-08-11 15:31:41 +0200

    Optimal Values

--- turboproject.py
+++ turboproject.py
@@ -150,7 +150,7 @@ print("\u03C7, \u03A6, \u03A8 = ", chi, phi, psi)
 # a * R_m - b / R_m = V_t1m
 # a * R_m + b / R_m = V_t2m
 
-n = 1
+n = 1.2
 matA = np.array([[R_m**n, -1 / R_m], 
                  [R_m**n,  1 / R_m]])
 
